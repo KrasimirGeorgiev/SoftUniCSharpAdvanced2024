0e20f5f2aafcccd4fc7862c288e231209d54bd05
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-10 01:39:45 +0300

    Finished excercise 3

--- 003. Maximum and Minimum Element/Program.cs
+++ 003. Maximum and Minimum Element/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        var stack = new Stack<int>();
+        for (int i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            var currentCommant = line[0];
+
+            switch (currentCommant)
+            {
+                case 1: stack.Push(line[1]); break;
+                case 2:
+                    if (stack.Count() > 0)
+                    {
+                        stack.Pop();
+                    }
+                    break;
+                case 3:
+                    if (stack.Count() > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                    break;
+                case 4:
+                    if (stack.Count() > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                    break;
+            }
+        }
+
+        if (stack.Count() > 0) 
+        {
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
