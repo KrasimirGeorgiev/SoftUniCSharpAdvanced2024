fd7f1663db2ab85e6729b635ce6776e69d9f5d9f
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-09 23:44:23 +0300

    Add project files.

---
+++ 002. BasicQueueOperations/Program.cs
@@ -0,0 +1,34 @@
+﻿var splitLine1 = Console.ReadLine().Split();
+var numbersToPushInToStack = int.Parse(splitLine1[0]);
+var numbersToPopFromTheStack = int.Parse(splitLine1[1]);
+var numberToLookFor = int.Parse(splitLine1[2]);
+
+var splitLine2 = Console.ReadLine().Split();
+
+var stack = new Stack<int>();
+
+var n = Math.Min(numbersToPushInToStack, splitLine2.Length);
+for (int i = 0; i < n; i++)
+{
+    var currentNumberToAdd = int.Parse(splitLine2[i]);
+    stack.Push(currentNumberToAdd);
+}
+
+while (numbersToPopFromTheStack > 0 && stack.Count() > 0)
+{
+    stack.Pop();
+    numbersToPopFromTheStack--;
+}
+
+if (stack.Contains(numberToLookFor))
+{
+    Console.WriteLine("true");
+}
+else if (stack.Count() == 0)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(stack.Min());
+}
