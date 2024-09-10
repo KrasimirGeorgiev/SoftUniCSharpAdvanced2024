bfa4d7e3a3b1a8bbdba6c3f0770f953ca7daf332
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-10 19:27:04 +0300

    Added 2 more excersices

---
+++ 004. Fast Food/Program.cs
@@ -0,0 +1,35 @@
+﻿var ordersCapacity = int.Parse(Console.ReadLine());
+var ordersForTheDay = Console.ReadLine()
+						.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+						.Select(int.Parse)
+						.ToArray();
+
+var leftOrders = new Queue<int>();
+var isDone = false;
+var maxOrderOfTheDay = int.MinValue;
+foreach (var order in ordersForTheDay)
+{
+	if (ordersCapacity - order >= 0 && !isDone)
+	{
+		ordersCapacity -= order;
+    }
+	else
+	{
+		leftOrders.Enqueue(order);
+		isDone = true;
+    }
+
+	maxOrderOfTheDay = Math.Max(maxOrderOfTheDay, order);
+}
+
+if (ordersForTheDay.Any())	
+	Console.WriteLine(maxOrderOfTheDay);
+
+if (leftOrders.Count > 0)
+{
+	Console.WriteLine("Orders left: " + string.Join(" ", leftOrders));
+}
+else
+{
+    Console.WriteLine("Orders complete");
+}
---
+++ 005. Fashion Boutique/Program.cs
@@ -0,0 +1,44 @@
+﻿var clothes = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+var capacityOfARack = int.Parse(Console.ReadLine());
+var stack = new Stack<int>();
+foreach (var item in clothes)
+{
+    stack.Push(item);
+}
+
+var numberOfRacks = 0;
+var currentRackNumber = capacityOfARack;
+if (stack.Peek() <= capacityOfARack)
+{
+    currentRackNumber -= stack.Pop();
+    numberOfRacks++;
+
+    foreach (var item in stack)
+    {
+        if (capacityOfARack - item < 0)
+            break;
+
+        if (currentRackNumber - item <= 0)
+        {
+            if (currentRackNumber - item == 0)
+            {
+                currentRackNumber = 0;
+            }
+            else
+            {
+                numberOfRacks++;
+                currentRackNumber = capacityOfARack - item;
+            }
+        }
+        else
+        {
+            currentRackNumber -= item;
+        }
+    }
+}
+
+Console.WriteLine(numberOfRacks);
