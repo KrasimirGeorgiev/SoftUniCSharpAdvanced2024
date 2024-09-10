4e6de8fd08b2133fa6db3dfab6973c0806c363ed
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-11 01:24:22 +0300

    Added excersice 6

---
+++ 006. Songs Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+var songs = Console.ReadLine()
+                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+var songQueue = new Queue<string>();
+foreach (var song in songs)
+{
+    songQueue.Enqueue(song);
+}
+
+while (songQueue.Any())
+{
+    var command = Console.ReadLine();
+    if (command == "Play")
+    {
+        songQueue.Dequeue();
+    }
+    else if (command.StartsWith("Add"))
+    {
+        var songName = command.Substring(4);
+        if (songQueue.Contains(songName))
+        {
+            Console.WriteLine($"{songName} is already contained!");
+        }
+        else
+        {
+            songQueue.Enqueue(songName);
+        }
+    }
+    else if (command == "Show")
+    {
+        Console.WriteLine(string.Join(", ", songQueue));
+    }
+}
+
+Console.WriteLine("No more songs!");
