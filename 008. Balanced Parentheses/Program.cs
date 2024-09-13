a436402e2fdf3648b15fdaf7310ec10a6b70a3a0
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-14 01:10:48 +0300

    Addin excercises

---
+++ 008. Balanced Parentheses/Program.cs
@@ -0,0 +1,42 @@
+﻿var inputLine = Console.ReadLine();
+
+var unclosedParentheses = new Stack<char>();
+var openParenthesesToClosing = new Dictionary<char, char>(){ { '(', ')'}, { '[', ']' }, { '{', '}' } };
+var closingParenthesesToOpening = new Dictionary<char, char>() { { ')', '(' }, { ']', '[' }, { '}', '{' } };
+
+foreach (var parenthesis in inputLine)
+{
+    var areUnclosedParentheses = unclosedParentheses.Any();
+    var isOpenParenthesis = openParenthesesToClosing.ContainsKey(parenthesis);
+    
+    // If there is no parantesis in the stack and the current one is a closing one
+    var isInputWrong = !areUnclosedParentheses && !isOpenParenthesis;
+    if (!isInputWrong)
+    {
+        // If there is an opening parantesis in the stack and the current parantesis is a closing one from a different type
+        if (areUnclosedParentheses && !isOpenParenthesis)
+        {
+            var lastParenthesis = unclosedParentheses.Peek();
+            var isLastParenthesisAnOpeningOne = openParenthesesToClosing.ContainsKey(lastParenthesis);
+            var isLastParenthesisSameType = openParenthesesToClosing.TryGetValue(lastParenthesis, out var closingParenthesis) && closingParenthesis == parenthesis;
+            if (isLastParenthesisSameType)
+                unclosedParentheses.Pop();
+            else
+                isInputWrong = true;
+        }
+        else
+        {
+            unclosedParentheses.Push(parenthesis);
+        }
+    }
+
+
+    if (isInputWrong)
+    {
+        unclosedParentheses.Push(parenthesis);
+        break;
+    }
+}
+
+var result = unclosedParentheses.Any() ? "NO" : "YES";
+Console.WriteLine(result);
---
+++ 009. SimpleTextEditor/Program.cs
@@ -0,0 +1,54 @@
+﻿
+var numberOfCommands = int.Parse(Console.ReadLine());
+
+var historyOfRecordAndDeleteOperations = new Stack<Tuple<OperationType, string>>();
+var resultString = string.Empty;
+for (int i = 0; i < numberOfCommands; i++)
+{
+    var currentCommand = Console.ReadLine().Split();
+    switch (currentCommand[0])
+    {
+        case "1":
+            resultString += currentCommand[1];
+            historyOfRecordAndDeleteOperations.Push(new Tuple<OperationType, string>(OperationType.Add, currentCommand[1].Length.ToString()));
+            break;
+        case "2":
+            var numberOfCharsToDelete = int.Parse(currentCommand[1]);
+            historyOfRecordAndDeleteOperations.Push(new Tuple<OperationType, string>(OperationType.Delete, resultString[(resultString.Length - numberOfCharsToDelete)..]));
+            resultString = resultString.Substring(0, resultString.Length - numberOfCharsToDelete);
+            break;
+        case "3":
+            Console.WriteLine(resultString[int.Parse(currentCommand[1]) - 1]);
+            break;
+        case "4":
+            resultString = UndoLastOperation(historyOfRecordAndDeleteOperations, resultString);
+            break;
+    }
+}
+
+static string UndoLastOperation(Stack<Tuple<OperationType, string>> operations, string resultString)
+{
+    if (operations.Any())
+    {
+        var lastOperation = operations.Pop();
+        var opositeOperation = (OperationType)(((int)lastOperation.Item1 + 1) % 2);
+        if (opositeOperation == OperationType.Add)
+        {
+            resultString += lastOperation.Item2;
+        }
+        else
+        {
+            resultString = resultString.Substring(0, resultString.Length - int.Parse(lastOperation.Item2));
+        }
+    }
+
+    return resultString;
+}
+
+enum OperationType
+{
+    Add,
+    Delete
+}
+
+
