5550d06fffc45319627f0ffdd38df7da2bd66727
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-14 20:45:03 +0300

    Solved excercise Key Revolver

---
+++ 011. Key Revolver/Program.cs
@@ -0,0 +1,46 @@
+﻿var amountPerBullet = int.Parse(Console.ReadLine());
+var sizeOfGunBarrel = int.Parse(Console.ReadLine());
+var bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+var locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+var amountOfTheReward = int.Parse(Console.ReadLine());
+var initialNumberOfBullets = bullets.Count();
+var initialNumberOfLocks = locks.Count();
+
+var currentAmountOfBullets = sizeOfGunBarrel;
+while (bullets.Any() && locks.Any())
+{
+    if (currentAmountOfBullets == 0)
+    {
+        Console.WriteLine("Reloading!");
+        currentAmountOfBullets = sizeOfGunBarrel;
+    }
+
+    var currentBullet = bullets.Pop();
+    var currentLock = locks.Peek();
+    if (currentBullet <= currentLock)
+    {
+        locks.Dequeue();
+        Console.WriteLine("Bang!");
+    }
+    else
+    {
+        Console.WriteLine("Ping!");
+    }
+
+    currentAmountOfBullets--;
+}
+
+if (locks.Any())
+{
+    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count()}");
+}
+else
+{
+    if (currentAmountOfBullets == 0 && bullets.Any())
+    {
+        Console.WriteLine("Reloading!");
+    }
+
+    var amountEarned = amountOfTheReward - ((initialNumberOfBullets - bullets.Count) * amountPerBullet);
+    Console.WriteLine($"{bullets.Count} bullets left. Earned ${amountEarned}");
+}
