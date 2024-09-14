aec795b94a2d9259b246fcb7acc7090814dc707e
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-15 00:52:41 +0300

    Solved excersice Cups and Bottles

---
+++ 012. Cups and Bottles/Program.cs
@@ -0,0 +1,33 @@
+﻿var cupsQueue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+var bottlesStack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+var initialCupsCount = cupsQueue.Count();
+var initialBottlesCount = bottlesStack.Count();
+var wastedWaterInLiters = 0;
+var currentCupValue = -1;
+while (cupsQueue.Any() && bottlesStack.Any())
+{
+    var currentCup = currentCupValue == -1 ? cupsQueue.Peek() : currentCupValue;
+    var currentBottle = bottlesStack.Pop();
+
+    if (currentBottle < currentCup)
+    {
+        currentCupValue = currentCup - currentBottle;
+    }
+    else
+    {
+        wastedWaterInLiters += currentBottle - currentCup;
+        cupsQueue.Dequeue();
+        currentCupValue = -1;
+    }
+}
+
+if (cupsQueue.Any())
+{
+    Console.WriteLine($"Cups: {string.Join(" ", cupsQueue)}");
+}
+else
+{
+    Console.WriteLine($"Bottles: {string.Join(" ", bottlesStack)}");
+}
+
+Console.WriteLine($"Wasted litters of water: {wastedWaterInLiters}");
