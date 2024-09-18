65839baa710815858523f6c9fa0a290fba01d887
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-18 20:46:43 +0300

    Added excersice 1 of the the homework for multidimentional arrays.

---
+++ 014. Diagonal Difference/Program.cs
@@ -0,0 +1,16 @@
+﻿var n = int.Parse(Console.ReadLine());
+var matrix = new int[n][];
+for (int i = 0; i < n; i++)
+{
+    matrix[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+}
+
+var firstDiagonal = 0;
+var secondDiagonal = 0;
+for (int i = 0; i < n; i++)
+{
+    firstDiagonal += matrix[i][i];
+    secondDiagonal += matrix[i][(n - 1) - i];
+}
+
+Console.WriteLine(Math.Abs(firstDiagonal - secondDiagonal));
