ace0691271a6b820345f9591fefa26bf6dfb05d2
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-09-29 00:27:35 +0300

    Added exam excercise 3. CocktailBar

---
+++ 913. Cocktail Bar/Cocktail.cs
@@ -0,0 +1,28 @@
+﻿namespace CocktailBar
+{
+    public class Cocktail
+    {
+        private List<string> _ingredients;
+
+        public Cocktail(string name, decimal price, double volume, string ingredients)
+        {
+            Name = name;
+            Price = price;
+            Volume = volume;
+            _ingredients = ingredients.Split(", ").ToList();
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public double Volume { get; set; }
+
+        public List<string> Ingredients { get => _ingredients; }
+
+        public override string ToString()
+        {
+            return $"{Name}, Price: {Price:F2} BGN, Volume: {Volume} ml{Environment.NewLine}Ingredients: {string.Join(", ", Ingredients)}";
+        }
+    }
+}
---
+++ 913. Cocktail Bar/Menu.cs
@@ -0,0 +1,56 @@
+﻿namespace CocktailBar
+{
+    public class Menu
+    {
+        private List<Cocktail> _cocktails;
+        public Menu(int barCapacity)
+        {
+            BarCapacity = barCapacity;
+            _cocktails = new List<Cocktail>();
+        }
+        public List<Cocktail> Cocktails { get => _cocktails; }
+
+        public int BarCapacity { get; set; }
+
+        public void AddCocktail(Cocktail cocktail)
+        {
+            if (_cocktails.Any(x => x.Name == cocktail.Name))
+            {
+                return;
+            }
+
+            if (_cocktails.Count < BarCapacity)
+            {
+                _cocktails.Add(cocktail);
+            }
+        }
+
+        public bool RemoveCocktail(string name)
+        {
+            Cocktail cocktail = _cocktails.FirstOrDefault(c => c.Name == name);
+            if (cocktail != null)
+            {
+                _cocktails.Remove(cocktail);
+                return true;
+            }
+            return false;
+        }
+
+        public Cocktail GetMostDiverse()
+        {
+            return _cocktails.OrderByDescending(c => c.Ingredients.Count).FirstOrDefault();
+        }
+
+        public string Details(string cocktailName)
+        {
+            return _cocktails.FirstOrDefault(x => x.Name == cocktailName)?.ToString();
+        }
+
+        public string GetAll()
+        {
+            return $"All Cocktails:" +
+               $"{Environment.NewLine}" +
+               $"{string.Join(Environment.NewLine, this.Cocktails.OrderBy(x => x.Name).Select(x => x.Name))}";
+        }
+    }
+}
