521566e8b49a22e0506b63098c80d6d7232a5787
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-10-02 00:47:38 +0300

    Added 2 excercises from sets and dictionaries

---
+++ 030. Unique Usernames/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+var n = int.Parse(Console.ReadLine());
+var set = new HashSet<string>();
+var sb = new StringBuilder();
+for (int i = 0; i < n; i++)
+{
+    var input = Console.ReadLine();
+    var isFirst = set.Add(input);
+    if (isFirst)
+    {
+        sb.AppendLine(input);
+    }
+}
+
+Console.WriteLine(sb.ToString());
---
+++ 031. Unique Usernames/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+var inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var list1 = new List<string>(inputs[0]);
+for (int i = 0; i < inputs[0]; i++)
+{
+    var input = Console.ReadLine();
+    list1.Add(input);
+}
+
+var list2 = new List<string>(inputs[1]);
+for (int i = 0; i < inputs[1]; i++)
+{
+    var input = Console.ReadLine();
+    list2.Add(input);
+}
+
+var set = list2.Distinct().ToHashSet();
+var sb = new StringBuilder();
+var resultList = new List<string>();
+foreach (var input in list1.Distinct())
+{
+    if (set.Contains(input))
+    {
+        resultList.Add(input);
+    }
+}
+
+Console.WriteLine(string.Join(" ", resultList));
