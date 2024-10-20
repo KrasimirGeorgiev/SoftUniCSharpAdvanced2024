0779883c4e891689c9542c329ab5a5299d55fbee
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-10-20 11:25:01 +0300

    Added test cases

--- 963. ExamTask3/Program.cs
+++ 963. ExamTask3/Program.cs
@@ -4,18 +4,60 @@
     {
         static void Main(string[] args)
         {
-            var creature = new Creature("Gorak", "Orc", 100, "Smash, Roar");
-            var creature2 = new Creature("Elora", "Elf", 80, "Heal, Arrow Shot");
-            var creature3 = new Creature("Drakon", "Dragon", 200, "Fire Breath");
-            var hub = new MythicalCreaturesHub(3);
-            hub.AddCreature(creature);
-            hub.AddCreature(creature2);
-            hub.AddCreature(creature3);
-            var asdf = hub.GetAllCreatures();
-            var actualResult = $"Mythical Creatures:{Environment.NewLine}Drakon -> Dragon{Environment.NewLine}Elora -> Elf{Environment.NewLine}Gorak -> Orc";
-            Console.WriteLine(asdf);
-            Console.WriteLine(actualResult);
-            Console.WriteLine(asdf == actualResult);
+            //Initialize the repository (MythicalCreaturesHub)
+            MythicalCreaturesHub creaturesHub = new MythicalCreaturesHub(3);
+
+            //Create some creatures
+            Creature orc = new Creature("Gorak", "Orc", 100, "Smash, Roar");
+            Creature elf = new Creature("Elora", "Elf", 80, "Heal, Arrow Shot");
+            Creature dragon = new Creature("Drakon", "Dragon", 200, "Fire Breath");
+
+            //Add creatures to the hub
+            creaturesHub.AddCreature(orc);
+
+            //The first creature is added, 2 more positions available
+            creaturesHub.AddCreature(elf);
+            creaturesHub.AddCreature(dragon);
+
+            //Attempt to add a creature when capacity is full
+            Creature goblin = new Creature("Zug", "Goblin", 50, "Steal, Dodge");
+            creaturesHub.AddCreature(goblin); //should not be added
+                                              //Attempt to add a creature with a duplicate name
+            Creature duplicateOrc = new Creature("Gorak", "Orc", 150, "Berserk");
+            creaturesHub.AddCreature(duplicateOrc); //should not be added
+
+            //Removing creatures from the hub
+            //Try to remove a not existing creature, should return False
+            Console.WriteLine(creaturesHub.RemoveCreature("Zug")); //False
+                                                                   //Try to remove an existing creature, should return True
+            Console.WriteLine(creaturesHub.RemoveCreature("Drakon")); //True
+
+            //Now it should be possible to add "Zug"
+            creaturesHub.AddCreature(goblin);
+
+            // Get the strongest creature
+            Console.WriteLine(creaturesHub.GetStrongestCreature());
+            //Output:
+            //Gorak (Orc) has 100 HP
+            //Abilities: Smash, Roar
+
+            //Getting existing creature details
+            Console.WriteLine(creaturesHub.Details("Elora"));
+            //Output:
+            //Elora (Elf) has 80 HP
+            //Abilities: Heal, Arrow Shot
+
+            //Getting NOT existing creature details
+            Console.WriteLine(creaturesHub.Details("Drakon"));
+            //Output:
+            //Creature with the name Drakon not found.
+            //Get all creatures’ names in alphabetical order
+            Console.WriteLine(creaturesHub.GetAllCreatures());
+            //Mythical Creatures:
+            //Elora -> Elf
+            //Gorak -> Ork
+            //Zug -> Goblin
+
         }
     }
 }
